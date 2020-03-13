49036ab46e08be7ae4217c1eed6df5d60b4ec4f4
mvaliadis <54821923+mvaliadis@users.noreply.github.com>
2020-03-13 09:27:36 +0100

    Adds: i. epoch extraction for classifier, ii. PSD calculation function

--- sleep_funs.py
+++ sleep_funs.py
@@ -13,6 +13,91 @@ import pandas as pd
 from reiz import clock
 import reiz
 import threading
+import mne
+from scipy.signal import welch
+
+def process_raw_EDF(fname):
+    raw_train = mne.io.read_raw_edf(fname + '-PSG.edf', preload=True)
+    
+    # load in hypnogram
+    annot_train = mne.read_annotations(fname + '-Hypnogram.edf')
+        
+    # filter data and select channels to use
+    raw_train.pick_types(include=(['EEG Fpz-Cz', 'EOG horizontal', 'EMG submental'])) 
+    raw_train.filter(0.5, 35, picks = ['EEG Fpz-Cz', 'EOG horizontal'])
+    raw_train.notch_filter(49)
+    raw_train.filter(20, 45, picks = (['EMG submental']))
+    
+    # select channels for annotations
+    mapping = {'EEG Fpz-Cz':'eeg',
+               'EOG horizontal': 'eog',
+               'EMG submental': 'emg'}
+     
+    raw_train.set_annotations(annot_train, emit_warning=False)
+    raw_train.set_channel_types(mapping)           
+       
+    # extract 30s events from annotations
+    annotation_desc_2_event_id = {'Sleep stage W': 1,
+                                  'Sleep stage 1': 2,
+                                  'Sleep stage 2': 3,
+                                  'Sleep stage 3': 4,
+                                  'Sleep stage 4': 4,
+                                  'Sleep stage R': 5}
+        
+    events_train, _ = mne.events_from_annotations(
+    raw_train, event_id=annotation_desc_2_event_id, chunk_duration=30.)
+        
+    # create a new event_id that unifies stages 3 and 4
+    event_id = {'Sleep stage W': 1,
+                'Sleep stage 1': 2,
+                'Sleep stage 2': 3,
+                'Sleep stage 3/4': 4,
+                'Sleep stage R': 5}
+
+    # create Epochs from the data based on the events found in the annotations
+    tmax = 30. - 1. / raw_train.info['sfreq']  # tmax in included
+        
+    epochs_train = mne.Epochs(raw=raw_train, events=events_train,
+                              event_id=event_id, tmin=0., tmax=tmax, baseline=None)
+            
+    datArray = epochs_train.get_data()*1e6
+    stageArray = epochs_train.events[:,2]
+    return datArray, stageArray
+
+def bandpower(epochs, fs):
+    """EEG relative power band feature extraction.
+
+    This function takes a numpy array of the data & creates EEG features based
+    on relative power in specific frequency bands that are compatible with
+    scikit-learn.
+
+    Parameters
+    ----------
+    epochs : Epochs
+        The data.
+
+    Returns
+    -------
+    X : numpy array of shape [n_samples, 5]
+        Transformed data.
+    """
+    # specific frequency bands
+    FREQ_BANDS = {"delta": [0.5, 4],
+                  "theta": [4, 8],
+                  "alpha": [8, 11],
+                  "sigma": [11, 16],
+                  "beta": [16, 30]}
+
+    freqs,psds = welch(epochs, fs = fs)
+    # Normalize the PSDs
+    psds /= np.sum(psds, axis=-1, keepdims=True)
+
+    X = []
+    for fmin, fmax in FREQ_BANDS.values():
+        psds_band = psds[:, :, (freqs >= fmin) & (freqs < fmax)].mean(axis=-1)
+        X.append(psds_band.reshape(len(psds), -1))
+
+    return np.concatenate(X, axis=1)
 
 class PinkNoise():
     def generate_noise(self,duration_in_s = 0.05, fs = 48000, ncols=16):
