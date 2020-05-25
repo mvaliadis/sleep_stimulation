73264c0e098c7cb5c75bc9c797783ec8bbfe9ac6
mvaliadis <54821923+mvaliadis@users.noreply.github.com>
2020-05-25 15:06:24 +0200

    update and clean threads

--- sleep_experiment.py
+++ sleep_experiment.py
@@ -4,99 +4,80 @@ Created on Mon Feb 17 09:36:25 2020
 
 @author: neuro
 """
-from os import chdir
-chdir('C:/Users/neuro/Documents/sleep_stimulation')
+
 import liesl
 import reiz.marker
-from sleep_funs import SO_detection, sleep_staging
+from sleep_funs import SO_detection, sleep_staging, subject_param_pull
 import matplotlib.pyplot as plt
 from reiz import clock
 import numpy as np
 import threading
 from scipy import signal
-# import yasa
+import yasa
+import pickle
 
-#%% set parameters
 
+#%% set parameters - only do for testing as excel file should contain proper info.
+## load subject code and condition script
+# file = 'subject_codes'
+# subjID = input('Please enter the correct subject ID: ')
+# evening = int(input(f'Please enter the correct recording session for the subject {subjID}: '))
+# time_delay, volume = subject_param_pull(file='subject_codes.csv', subjID, evening)
 
-#%% set up EEG and marker stream
+time_delay = .5#float(input('Please select the participants average peak to peak SO amplitude from the adaption evening. '))
+volume = 1#int(input('Please select the volume for the experiment, either 0 or 1. '))
+
+#%% set up EEG and marker stream for testing mock data
 #-----------------------------
 #----Tip: for development and testing, you can just set up a fake EEG stream by
 #----entering the following into your command line:
 #----liesl mock --type EEG
+#----Alternatively, a replay stream can be set-up to test already recorded data,
+#----to determine accuracy of detection, by entering the following:
+#----python -m replay --file <path_to_xdf_file>
 
-
-#%% load subject code and condition script
-# subj_cond = np.loadtxt('subject_codes.csv',delimiter=',', dtype='str', skiprows=1)
-
-# ## determine stimulation condition based on recording evening and subject code
-# # to-do later: conditions need to be encrypted at creation point and decrypted here 
-# cond = int(input('please enter experimental recording evening: ')) 
-# index_pos = []
-# # obtain index position of subject from excel file
-# while index_pos == []:
-#     # to-do later: might be a more elegant way to obtain this
-#     subj = input('Please enter the subject token: ')
-#     index_pos = [i for i,item in enumerate(subj_cond) if subj in item]
-#     if index_pos == []:
-#         print('Subject code is invalid, please enter a valid subject code!')
-#     else:    
-#         ## time delay + volume based on condition
-#         # 0 corresponds to sham (same trigger as up, without volume)
-#         if int(subj_cond[index_pos[0],:][cond]) == 0:
-#             time_delay = .500
-#             volume = 0
-#         # 1 corresponds to up-state targeting 
-#         elif int(subj_cond[index_pos[0],:][cond]) == 1:
-#             time_delay = .500
-#             volume = 1
-#         # 2 corresponds to down-state targeting     
-#         elif int(subj_cond[index_pos[0],:][cond]) == 2:
-#             time_delay = 0
-#             volume = 1 
-
-     
-        
+    
 #%% put sleep stager into separate thread that can run in the background
-
 def main():
 
-    sinfo = liesl.get_streaminfos_matching(type = 'EEG')
-    bfr = liesl.RingBuffer(sinfo[0], duration_in_ms = 30000) #30 s buffer to allow sleep staging
-    bfr.start()
-    
-    bfr.await_running()
-    sleep_stager = threading.Thread(target = sleep_staging, args = (bfr,))
-    
-    # start thread
-    sleep_stager.start()
+     
+     # get sample info and pull data ringbuffer
+     sinfo = liesl.get_streaminfos_matching(type = 'EEG')
+     bfr = liesl.RingBuffer(sinfo[0], duration_in_ms = 30000) #30 s buffer to allow sleep staging
+     bfr.start()
 
-   
-    #%% initialize second thread for SW detection
+     bfr.await_running()
+     reiz.clock.sleep(5)
+     sleep_stager = threading.Thread(target = sleep_staging, args = (bfr,))
 
-    SO_detection()
+     # start thread
+     sleep_stager.start()
+
+
+     # initialize second thread for SW detection
+     SO_detection(time_delay, volume)
+     
+     # sleep for the remainder of the evening or until participant awakens
+     print('Sleep time! The recording will continue, although the stimulation paradigm has officially ended!')
+     reiz.clock.sleep(18000) 
+
     
 
-    
 
 if __name__ == '__main__':
     from liesl.files.session import Session
-    subj_id = input('Enter Subject ID... ')
-    streamargs = [{"name":"eeg_replay"}, {"name":"reiz-marker"}]
+    #reiz.marker.start()
+    subj_id = subjID
+    streamargs = [{"name":"eego"}, {"name":"reiz-marker"}]
+    #streamargs = [{'type':'EEG','type':'Markers'}]
     mainfolder = 'C:/Users/neuro/Documents/sleep_stimulation/recordings'
+    #mainfolder = 'C:/Users/mvali/Documents/Sleep_classification/recordings'
     session = Session(prefix = subj_id, streamargs = streamargs, mainfolder = mainfolder)
-
-    global stage_predictArrays
-    stage_predictArrays = []
-    
+     
     with session('sleepstim'):
         main()
-
-## to-do: test timing of integrated functions 
-## to-do: take nearest channels in case of failure for detection/classifier, take out EOG/EMG if they fail
-## to-do: write channel failure function accounting for both signal quality reduction and non-physio channel activity
-## to-do: train classifier on different streams (EEG,EOG,EMG)
-
-## to-do: stage sleep 
-## DONE: move functions into function script
-## to-do: encrypt/decrypt subject codes
+      
+## to-do: test timing of integrated functions in simulation 
+## to-do: increase sensitivity of classifier
+## to-do: pilot testing
+## to-do: finalize docstrings
