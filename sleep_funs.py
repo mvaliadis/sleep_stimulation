6fe97d2142dae1864de2641c14a04641ae9c2ffa
mvaliadis <54821923+mvaliadis@users.noreply.github.com>
2020-08-11 16:23:27 +0200

    Add transitional probability calculation + plot funcs

--- sleep_funs.py
+++ sleep_funs.py
@@ -435,6 +435,45 @@ def ROC_curve_plot(rf_roc_auc, fpr, tpr, thresholds):
     
 #     return X_train, X_test
 
+def transition_matrix(transitions):
+    # the function takes a list with states labeled as successive integers and
+    # returns a transition matrix, trans_max of all transitions between given states
+    
+    # derive number of states
+    states = len(np.unique(transitions)) 
+    
+    # initialize matrix with 0s
+    trans_matrix = [[0]*states for _ in range(states)] 
+    
+    # extract instances of each state
+    for (i,j) in zip(transitions,transitions[1:]):
+        trans_matrix[i][j] += 1
+        
+    return trans_matrix
+
+def transition_matrix_prob(trans_matrix):
+    # convert occurences to a transitional probability matrix to indicate the 
+    # probability of transitioning from one state to the next
+    probs = []   
+    for row in trans_matrix:
+        prob = row / row.sum(axis=-1, keepdims=True)
+        probs.append(prob)
+        
+    return np.round(np.array(probs).astype(float), 4) 
+ 
+def transition_matrix_plot(probs):
+    grid_kws = {"height_ratios": (.9, .05), "hspace": .1}
+    f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws, figsize=(5, 5))
+    sns.heatmap(probs, ax=ax, square=False, vmin=0, vmax=1, cbar=True,
+                cbar_ax=cbar_ax, cmap='YlOrRd', annot=True, fmt='.2f',
+                cbar_kws={"orientation": "horizontal", "fraction": 0.1,
+                          "label": "Transition probability"})
+    ax.set_xlabel("To sleep stage")
+    ax.xaxis.tick_top()
+    ax.set_ylabel("From sleep stage")
+    ax.xaxis.set_label_position('top')
+    # plt.savefig('transition.png', dpi=100, bbox_inches='tight')
+    
 #%%
 ## Subject code based parameter pulling function
 
