6c112e40a3710b0ab6ad78083d45c6d8ae6566b9
mvaliadis <54821923+mvaliadis@users.noreply.github.com>
2021-02-15 12:27:00 +0100

    initial setup file commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+    name='sleep_stimulation',
+    version='0.0.1',
+    description='Closed-loop sleep stimulation toolbox to process data in real time via LSL stream interface.',
+    long_description='A Python toolbox that allows for online interface with LSL streams. The toolbox also includes a sleep classification framework for training data for online closed-loop gateing. Lastly, the packagae contains functions and pipelines for offline preprocessing and analysis.',
+    author='Michael Valiadis',
+    author_email='mvaliadis2@gmail.com',
+    packages=['sleep_stimulation'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries',
+        ]
+)
